7cbabc4e074d65270d7ef407dc4f5bf15ca5070d
santitham <santitham.anan@gmail.com>
2018-08-05 04:57:44 +0700

    add dummy key api

--- routes/api/key.js
+++ routes/api/key.js
@@ -1,39 +1,20 @@
 import crypto from 'crypto'
 import fs from 'fs'
-import ursa from 'ursa'
 
-const router = express.Router();
+router.get('/key', (req, res) => {
+  const private1 = fs.readFileSync('../../key/user-1/id_rsa', 'utf-8')
+  const public1 = fs.readFileSync('../../key/user-1/id_rsa.pub', 'utf-8')
+  const private2 = fs.readFileSync('../../key/user-2/id_rsa', 'utf-8')
+  const public2 = fs.readFileSync('../../key/user-2/id_rsa.pub', 'utf-8')
 
-router.post('/message', (req, res) => {
-  // privateKeyPath = '../../crypto/private/'
-  const private_key = fs.readFileSync('private.pem', 'utf-8').toString();
-  const private_key = fs.readFileSync('../../crypto/private/private.pem', 'utf-8')
-  const public_key = fs.readFileSync('../../crypto/public/public.pub', 'utf-8')
-  const message = 'test'
-  
-  const signer = crypto.createSign('sha256');
-  signer.update(message);
-  signer.end();
-  
-  const signature = signer.sign(private_key)
-  const signature_hex = signature.toString('hex')
-  
-  const verifier = crypto.createVerify('sha256');
-  verifier.update(message);
-  verifier.end();
-  
-  const verified = verifier.verify(public_key, signature_hex, 'hex');
-  
-  res.json({
-    message: message,
-    signature: signature_hex,
-    verified: verified
+  return res.json({
+    '1': {
+      publicKey: public1,
+      privateKey: private1
+    },
+    '2': {
+      publicKey: public2,
+      privateKey: private2
+    }
   })
-  console.log(JSON.stringify({
-      message: message,
-      signature: signature_hex,
-      verified: verified,
-  }, null, 2));  
 });
-
-export default router;
