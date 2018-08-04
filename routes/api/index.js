9ac9a307ed03ee6e899916b158912a5434f0f9e6
santitham <santitham.anan@gmail.com>
2018-08-05 02:27:05 +0700

    add key

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 import login from './login';
+import key from './key';
 
 const router = express.Router();
 
@@ -9,8 +10,9 @@ router.get('/', (req, res) => {
   res.json({
     title: 'Api'
   });
-});
+})
 
 router.use('/login', login);
+router.use('/key', key);
 
 export default router;
--- routes/api/login.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-
-router.post('/', (req, res) => {
-  res.json(req.body);
-});
-
-export default router;
